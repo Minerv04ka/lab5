d423a57aee53cb8512c42455181bbabecfb01cb9
Minerv04ka <oleksandr.zhuk.2000@gmail.com>
2023-11-08 19:34:08 +0200

    first commit

---
+++ Lab_5/Program.cs
@@ -0,0 +1,43 @@
+﻿//Var 6
+using System;
+
+public class Cube
+{
+    private double sideLength;
+
+    public double SideLength
+    {
+        get { return sideLength; }
+        set
+        {
+            if (value >= 0)
+                sideLength = value;
+        }
+    }
+
+    public double CalculateVolume()
+    {
+        return sideLength * sideLength * sideLength;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cube cube = new Cube();
+
+        double side;
+        Console.WriteLine("Введіть довжину ребра куба: ");
+
+        while (!double.TryParse(Console.ReadLine(), out side) || side < 0)
+        {
+            Console.WriteLine("Введено некоректне значення. Введіть будь ласка додатнє число.");
+            Console.Write("Введіть довжину ребра куба: ");
+        }
+
+        cube.SideLength = side;
+
+        Console.WriteLine($"Об'єм куба з ребром {cube.SideLength} дорівнює {cube.CalculateVolume():F2}");
+    }
+}
